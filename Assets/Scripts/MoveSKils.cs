7b735bf9e498537d135aa1b053d5cd76e2a4bc98
Boltivegull18 <alvaromele@hotmail.com>
2025-01-23 00:18:36 -0600

    BlendTree
    agrege el blend tree y estoy viendo k chow con las skills

---
+++ Assets/Scripts/MoveSKils.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using UnityEngine;
+
+public class SkillsPlayer : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public GameObject Lanzar;
+
+    public int Wrap;
+    public float Burbuja;
+    private Animator animPibe;
+    // Start is called before the first frame update
+    void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+         animPibe = GetComponent<Animator>();
+    }
+        // Update is called once per frame
+    
+    void Update(){
+        // Obtener la entrada del usuario (WASD o flechas)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animPibe.SetFloat("X",movement.x);
+        animPibe.SetFloat("Y",movement.y);
+    }
+    void FixedUpdate()
+    {
+        // Mover al personaje usando la física
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    void EnvuelvePibes()
+    {
+        Input.GetKey("Z");
+
+    }
+    void AtontaPibes()
+    {
+        Input.GetKey("X");
+        
+
+
+    }
+
+    void AvientaPibes()
+    {
+        Input.GetKey("C");
+        GameObject.Instantiate(Lanzar);
+        
+    }
+
+
+    
+}
